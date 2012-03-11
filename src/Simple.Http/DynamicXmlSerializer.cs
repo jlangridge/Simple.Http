f07bf1de79c112bb1f153913d798cd30bec91a8b
James Langridge <j.langridge@nature.com>
2012-03-11 13:20:21 +0000

    Completed *very* basic serialization; very simple, not nested XML only, and nothing but ints and strings

--- src/Simple.Http/DynamicXmlSerializer.cs
+++ src/Simple.Http/DynamicXmlSerializer.cs
@@ -1,25 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 
-namespace Simple.Http.Spike
+namespace Simple.Http
 {
+    /// <summary>
+    ///   Serializes and deserializes dynamic objects into and from XML documents.
+    /// </summary>
     public class DynamicXmlSerializer : XmlSerializer
     {
-       
+        /// <summary>
+        ///   Deserializes the XML document contained by the specified System.Xml.XmlReader
+        /// </summary>
+        /// <param name="reader"> The System.Xml.XmlReader that contains the XML document to deserialize </param>
+        /// <returns> A <see cref="System.Dynamic">dynamic</see> object containing the results of the serialization </returns>
         public new dynamic Deserialize(XmlReader reader)
         {
             var xmlDoc = XDocument.Load(reader);
 
+            if (xmlDoc.Root == null)
+            {
+                throw new ArgumentException("Reader does not point to a valid XML document", "reader");
+            }
+
             dynamic result = new ExpandoObject();
-            result.Id = int.Parse(xmlDoc.Root.Element("id").Value);
-            result.Description = xmlDoc.Root.Element("description").Value;
+            var resultInfo = (IDictionary<string, object>) result;
+
+            foreach (var element in xmlDoc.Root.Elements())
+            {
+                int i;
+                object value;
+                if (int.TryParse(element.Value, out i))
+                {
+                    value = i;
+                }
+                else
+                {
+                    value = element.Value;
+                }
+                var name = CodeIdentifier.MakePascal(element.Name.ToString());
+                resultInfo[name] = value;
+            }
+
             return result;
         }
     }
--- tests/Simple.Http.Tests/DynamicXmlSerializerTests.cs
+++ tests/Simple.Http.Tests/DynamicXmlSerializerTests.cs
@@ -1,24 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 using System.Xml;
 using NUnit.Framework;
-using Simple.Http.Spike;
+
 
 namespace Simple.Http.Tests
 {
     [TestFixture]
     public class DynamicXmlSerializerTests
     {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            Reader = XmlReader.Create(new StringReader("<post><id>5</id><description>test</description></post>"));
+        }
+
+        #endregion
+
+        private XmlReader Reader { get; set; }
+
         [Test]
         public void DeserializeShouldReturnACorrectlyPopulatedDynamicObject()
         {
-            var reader = XmlReader.Create(new StringReader("<post><id>5</id><description>test</description></post>"));
             var serializer = new DynamicXmlSerializer();
-            var result = serializer.Deserialize(reader);
-            Assert.AreEqual(5, result.Id);
+            var result = serializer.Deserialize(Reader);
+            Assert.AreEqual(5, result.ID);
+        }
+
+        [Test]
+        public void DeserializeShouldReturnObjectsWithCorrectlyParsedStringProperties()
+        {
+            var serializer = new DynamicXmlSerializer();
+            var result = serializer.Deserialize(Reader);
+            Assert.AreEqual("test", result.Description);
         }
     }
 }
